e25ed2c206c19ea099ca532b971c7b447e7618f3
Danila Safonov <danila24safonov@mail.ru>
2023-10-18 12:20:17 +0300

    Write API and interfaces for DB and ML

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
-
+import requests
 app = FastAPI()
 
+class IDbService:
+    def saveFile(self, data, x, y):
+        ...
+
+    def getPoints(self):
+        ...
+
+class IMlService:
+    def getData(self, image):
+        ...
+
+service = IDbService()
+ml = IMlService
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    points = service.getPoints()
+    return points
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/")
+async def say_hello(url: str, x: int, y: int):
+    image = requests.get(url)
+    image = ml.getData(image)
+    service.saveFile(image, x, y)
+    return image
